d8a66c9cdaded0d267fefdae176852baffb81550
shiningdragon <speakwithjoe@hotmail.co.uk>
2015-07-29 11:19:23 +0200

    Restructure for better azure hosting

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 
 var server = express();
-server.use(express.static(__dirname));  // this just makes all our content servable - use sub folders if you need more control
+server.use(express.static(__dirname+ '/public'));  // this just makes all our content servable - use sub folders if you need more control
 
 var port = process.env.PORT || 1337
 server.listen(port, function () {
