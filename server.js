1f037bcf850fb26930dd8ba2fcaeb0e936a2c07a
shiningdragon <speakwithjoe@hotmail.co.uk>
2015-07-29 10:59:45 +0200

    Update node to allow azure hosting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var server = express();
 server.use(express.static(__dirname));  // this just makes all our content servable - use sub folders if you need more control
 
-var port = 80;
+var port = process.env.PORT || 1337
 server.listen(port, function () {
     console.log('server listening on port ' + port);
 });
